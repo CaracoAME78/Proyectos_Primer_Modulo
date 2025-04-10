0e0099218fd6ba658bfd4ea01bb80ace0b240add
Carlos Ramos <carlosrc78@gmail.com>
2025-04-09 20:04:21 -0500

    Agregar archivos de proyecto.

---
+++ Pry-ConvertidorUnidades/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pry_ConvertidorUnidades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
